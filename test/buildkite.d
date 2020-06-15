94849fca11fc5bca18bd1dd489b76054b9809bbf
Sebastian Wilzbach <seb@wilzba.ch>
2020-06-15 11:35:28 +0200

    Remove scaleway

--- test/buildkite.d
+++ test/buildkite.d
@@ -14,29 +14,6 @@ unittest
     postBuildkiteHook("ping.json");
 }
 
-//==============================================================================
-// Release-Builders on Scaleway (Bare Metal)
-//==============================================================================
-
-Json scwCreateServerResp(string id, string name)
-{
-    import core.time : Duration;
-    import std.datetime.timezone : SimpleTimeZone;
-    import std.file : readText;
-    import vibe.data.json : parseJsonString;
-
-    // use zulu to get +00:00 instead of Z suffix
-    static zulu = new immutable SimpleTimeZone(Duration.zero, "Etc/Zulu");
-    auto time = now(zulu).toISOExtString;
-
-    auto json = "data/payloads/scaleway_servers_post".readText.parseJsonString;
-    json["server"]["id"] = id;
-    json["server"]["name"] = name;
-    json["server"]["state"] = "stopped";
-    json["server"]["creation_date"] = time;
-    return json;
-}
-
 @("spawns-release-builder")
 unittest
 {
@@ -45,26 +22,7 @@ unittest
             j.findPipeline("build-release")["scheduledBuilds"]["edges"] ~=
                 ["node": ["branch": "master"]].serializeToJson;
         }),
-        "/hcloud/servers",
-        "/scaleway/servers", (ref Json j) {
-            j["servers"] = Json.emptyArray;
-        },
-        "/scaleway/images",
-        "/scaleway/servers",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.POST);
-            assert(req.json["name"].get!string.startsWith("release-builder-"));
-            auto name = req.json["name"].get!string;
-            assert(req.json["image"] == "487de557-88fe-4029-a404-831c19744ebd");
-            assert(req.json["organization"] == "aa435976-67f1-455c-b988-f4dc04c91f40");
-            res.writeJsonBody(scwCreateServerResp("a4919456-92a2-4cab-b503-ca13aa14c786", name));
-        },
-        "/scaleway/servers/a4919456-92a2-4cab-b503-ca13aa14c786/action",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.POST);
-            assert(req.json["action"] == "poweron");
-            res.writeBody("");
-        }
+        "/hcloud/servers"
     );
 
     postBuildkiteHook("build_scheduled_build-release.json");
@@ -79,7 +37,6 @@ unittest
                 ["node": ["branch": "master"]].serializeToJson;
         }),
         "/hcloud/servers",
-        "/scaleway/servers",
     );
 
     postBuildkiteHook("build_scheduled_build-release.json");
@@ -96,23 +53,6 @@ unittest
                 ["node": ["startedAt": (now - 30.minutes).toISOExtString]].serializeToJson;
         }),
         "/hcloud/servers",
-        "/scaleway/servers",
-        "/scaleway/images",
-        "/scaleway/servers",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.POST);
-            assert(req.json["name"].get!string.startsWith("release-builder-"));
-            auto name = req.json["name"].get!string;
-            assert(req.json["image"] == "487de557-88fe-4029-a404-831c19744ebd");
-            assert(req.json["organization"] == "aa435976-67f1-455c-b988-f4dc04c91f40");
-            res.writeJsonBody(scwCreateServerResp("c9660dc8-cdd9-426c-99c8-1155a568d53e", name));
-        },
-        "/scaleway/servers/c9660dc8-cdd9-426c-99c8-1155a568d53e/action",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.POST);
-            assert(req.json["action"] == "poweron");
-            res.writeBody("");
-        }
     );
 
     postBuildkiteHook("build_scheduled_build-release.json");
@@ -128,37 +68,6 @@ unittest
             j.findAgents("release-builder-89cd08ef-e418-4b0f-9453-7a218662fbdb").front["node"]["connectionState"] = "lost";
         }),
         "/hcloud/servers",
-        "/scaleway/servers", (ref Json j) {
-            j["servers"][0]["state"] = "stopped";
-        },
-        "/scaleway/servers/a4919456-92a2-4cab-b503-ca13aa14c786",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.DELETE);
-            res.statusCode = 204;
-            res.writeBody("");
-        },
-        "/scaleway/volumes/33e87570-fbf6-40f8-9afd-af77fadf781a",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.DELETE);
-            res.statusCode = 204;
-            res.writeBody("");
-        },
-        "/scaleway/images",
-        "/scaleway/servers",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.POST);
-            assert(req.json["name"].get!string.startsWith("release-builder-"));
-            auto name = req.json["name"].get!string;
-            assert(req.json["image"] == "487de557-88fe-4029-a404-831c19744ebd");
-            assert(req.json["organization"] == "aa435976-67f1-455c-b988-f4dc04c91f40");
-            res.writeJsonBody(scwCreateServerResp("c9660dc8-cdd9-426c-99c8-1155a568d53e", name));
-        },
-        "/scaleway/servers/c9660dc8-cdd9-426c-99c8-1155a568d53e/action",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.POST);
-            assert(req.json["action"] == "poweron");
-            res.writeBody("");
-        }
     );
 
     postBuildkiteHook("build_scheduled_build-release.json");
@@ -205,7 +114,6 @@ unittest
         "/hcloud/servers", (ref Json j) {
             j["servers"] = Json.emptyArray;
         },
-        "/scaleway/servers",
         "/hcloud/images?sort=created:desc&type=snapshot",
         "/hcloud/servers",
         (scope HTTPServerRequest req, scope HTTPServerResponse res) {
@@ -229,7 +137,6 @@ unittest
                 ["node": ["branch": "master"]].serializeToJson;
         }),
         "/hcloud/servers",
-        "/scaleway/servers",
     );
 
     postBuildkiteHook("build_scheduled_dmd.json");
@@ -248,7 +155,6 @@ unittest
                 ["node": ["branch": "MartinNowak:fix19337"]].serializeToJson;
         }),
         "/hcloud/servers",
-        "/scaleway/servers",
         "/hcloud/images?sort=created:desc&type=snapshot",
         "/hcloud/servers",
         (scope HTTPServerRequest req, scope HTTPServerResponse res) {
@@ -274,7 +180,6 @@ unittest
                 ["node": ["startedAt": (now - 5.minutes).toISOExtString]].serializeToJson;
         }),
         "/hcloud/servers",
-        "/scaleway/servers",
     );
 
     postBuildkiteHook("build_scheduled_dmd.json");
@@ -290,9 +195,6 @@ unittest
             j["servers"][0]["id"] = 1321993;
             j["servers"][0]["name"] = "ci-agent-without-bk-agent";
             j["servers"][0]["created"] = (now - 30.minutes).toISOExtString;
-        },
-        "/scaleway/servers", (ref Json j) {
-            j["servers"] = Json.emptyArray;
         },
         "/hcloud/servers/1321993",
         (scope HTTPServerRequest req, scope HTTPServerResponse res) {
@@ -323,16 +225,6 @@ unittest
     setAPIExpectations(
         "/buildkite", &graphQL!"buildkite_organization",
         "/hcloud/servers",
-        "/scaleway/servers", (ref Json j) {
-            j["servers"][0]["name"] = "release-builder-123456";
-            j["servers"][0]["id"] = "a4919456-92a2-4cab-b503-ca13aa14c786";
-        },
-        "/scaleway/servers/a4919456-92a2-4cab-b503-ca13aa14c786/action",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.POST);
-            assert(req.json["action"] == "terminate");
-            res.writeBody("");
-        }
     );
 
     postAgentShutdownCheck("release-builder-123456");
@@ -347,7 +239,6 @@ unittest
             j["servers"][0]["name"] = "ci-agent-123456";
             j["servers"][0]["id"] = 1321993;
         },
-        "/scaleway/servers",
         "/hcloud/servers/1321993",
         (scope HTTPServerRequest req, scope HTTPServerResponse res) {
             assert(req.method == HTTPMethod.DELETE);
@@ -367,7 +258,6 @@ unittest
                 ["node": ["startedAt": (now - 30.minutes).toISOExtString]].serializeToJson;
         }),
         "/hcloud/servers",
-        "/scaleway/servers",
     );
 
     postAgentShutdownCheck("release-builder-123456");
@@ -382,7 +272,6 @@ unittest
                 ["node": ["branch": "master"]].serializeToJson;
         }),
         "/hcloud/servers",
-        "/scaleway/servers",
     );
 
     postAgentShutdownCheck("ci-agent-123456");
@@ -418,20 +307,9 @@ unittest
             j["servers"][1]["name"] = "ci-agent-93faf8ca-6633-4b96-9abb-3d9cb1c4018e";
             j["servers"][1]["created"] = (now - 40.minutes).toISOExtString;
         },
-        "/scaleway/servers", (ref Json j) {
-            j["servers"][0]["id"] = "a4919456-92a2-4cab-b503-ca13aa14c786";
-            j["servers"][0]["name"] = "release-builder-89cd08ef-e418-4b0f-9453-7a218662fbdb";
-            j["servers"][0]["creation_date"] = (now - 60.minutes).toISOExtString;
-        },
         "/hcloud/servers/1321993",
         (scope HTTPServerRequest req, scope HTTPServerResponse res) {
             assert(req.method == HTTPMethod.DELETE);
-            res.writeBody("");
-        },
-        "/scaleway/servers/a4919456-92a2-4cab-b503-ca13aa14c786/action",
-        (scope HTTPServerRequest req, scope HTTPServerResponse res) {
-            assert(req.method == HTTPMethod.POST);
-            assert(req.json["action"] == "terminate");
             res.writeBody("");
         },
     );
@@ -457,11 +335,6 @@ unittest
             j["servers"][1]["id"] = 1321994;
             j["servers"][1]["name"] = "ci-agent-93faf8ca-6633-4b96-9abb-3d9cb1c4018e";
             j["servers"][1]["created"] = (now - 40.minutes).toISOExtString;
-        },
-        "/scaleway/servers", (ref Json j) {
-            j["servers"][0]["id"] = "a4919456-92a2-4cab-b503-ca13aa14c786";
-            j["servers"][0]["name"] = "release-builder-89cd08ef-e418-4b0f-9453-7a218662fbdb";
-            j["servers"][0]["creation_date"] = (now - 60.minutes).toISOExtString;
         },
     );
 
