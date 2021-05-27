01481b087113fc6c6eed9e932ca67aeb9d7f4ee2
Vladimir Panteleev <git@cy.md>
2021-05-27 02:00:09 +0000

    dlangbot.github_api: Fix GitHub paging
    "prev" occurring before "next" was tripping up the old parser logic.

--- test/github.d
+++ test/github.d
@@ -23,3 +23,39 @@ unittest
     assert(ghGetRequest(githubAPIURL ~ "/test").body == "hello");
     assert(ghGetRequest(githubAPIURL ~ "/test").body == "hello");
 }
+
+@("github-multipage")
+unittest
+{
+    setAPIExpectations(
+        "/github/multipage-test",
+        (scope HTTPServerRequest req, scope HTTPServerResponse res){
+            res.headers["Link"] =
+				`<` ~ githubAPIURL ~ `/multipage-test?page=2>; rel="next", ` ~
+				`<` ~ githubAPIURL ~ `/multipage-test?page=3>; rel="last"`;
+            res.writeJsonBody("D".Json);
+        },
+        "/github/multipage-test?page=2",
+        (scope HTTPServerRequest req, scope HTTPServerResponse res){
+            res.headers["Link"] =
+				`<` ~ githubAPIURL ~ `/multipage-test?page=1>; rel="prev", ` ~
+				`<` ~ githubAPIURL ~ `/multipage-test?page=3>; rel="next", ` ~
+				`<` ~ githubAPIURL ~ `/multipage-test?page=3>; rel="last", ` ~
+				`<` ~ githubAPIURL ~ `/multipage-test?page=1>; rel="first"`;
+            res.writeJsonBody("is".Json);
+        },
+        "/github/multipage-test?page=3",
+        (scope HTTPServerRequest req, scope HTTPServerResponse res){
+            res.headers["Link"] =
+				`<` ~ githubAPIURL ~ `/multipage-test?page=2>; rel="prev", ` ~
+				`<` ~ githubAPIURL ~ `/multipage-test?page=1>; rel="first"`;
+            res.writeJsonBody("awesome".Json);
+        },
+	);
+
+    auto pages = ghGetAllPages(githubAPIURL ~ "/multipage-test");
+	string result;
+    foreach (page; pages)
+		result ~= page.get!string;
+	assert(result == "D" ~ "is" ~ "awesome");
+}
