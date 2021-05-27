e03480e836fac835bb2d0fdb461c7fdaa93135ab
Vladimir Panteleev <git@cy.md>
2021-05-27 20:04:18 +0000

    dlangbot.github_api: Add PullRequest.diffURL

--- source/dlangbot/github_api.d
+++ source/dlangbot/github_api.d
@@ -242,6 +242,7 @@ struct PullRequest
     @name("created_at") SysTime createdAt;
     @name("updated_at") SysTime updatedAt;
     @name("closed_at") Nullable!SysTime closedAt;
+    @name("diff_url") string diffURL;
     bool locked;
     // TODO: update payloads
     //@name("maintainer_can_modify") bool maintainerCanModify;
